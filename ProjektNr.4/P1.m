f324b82b2cb7cf3c4d67578e2c8d0c5979f5e692
Kajetan Kaczmarek <kajkacz@wp.pl>
2018-06-02 23:14:16 +0200

    Part of 4th project

--- ProjektNr.4/P1.m
+++ ProjektNr.4/P1.m
@@ -1,21 +1,29 @@
 %% Given of the project %%
-step = 0.02 ;
+step = 0.2 ;
 start = 0;
-range = 20;                                           
+range = 20;
+
+max = range/step + 1;
+y1 = zeros(max,4);
+y2 = zeros(max,4);
+
 F1 = @(x1,x2) x2 + x1*(0.9 - x1*x1 - x2*x2);   
 F2 = @(x1,x2) -x1 + x2*(0.9 - x1*x1 - x2*x2);   
+
 x1 = [10 0 8 0.001];
 x2 = [8 9 0 0.001];
 
 X = [start:step:range];
 
 for i = 1:4
-    [ y1 , y2 ] = RungyKutta(step,range,F1,F2,x1(i), x2(i));
-    [ t , y] = ode45('FF1',[0 20],[x1(i)0 x2(i)]);
+    [ y1(:,i) , y2(:,i) ] = RungyKutta(step,range,F1,F2,x1(i), x2(i));
+    %[ t , y] = ode45('FF1',[0 20],[x1(i) x2(i)]);
 
 end
     hold on
-    plot(y(:,1),y(:,2))
-    plot(X,y1)
-    plot(X,y2)
+   % plot(y(:,1),y(:,2))
+    plot(y1(:,i),y2(:,i))
+    %plot(X,y1(:,i))
+    %plot(X,y2(:,i))
     hold off
+    
--- ProjektNr.4/RungyKutta.m
+++ ProjektNr.4/RungyKutta.m
@@ -6,24 +6,28 @@ function [y1,y2] = RungyKutta(step,range,F1,F2,x1, x2)
 % F1, F2 - our given differentials
 % x1, x2 - our given starting points
 %%%%%%%%%%%%%%
+max = range/step;
+
+y1 = zeros(max,1);
+y2 = zeros(max,1);
+
 y1(1) = x1;                               % initial condition
 y2(1) = x2;
 
-max = 50;% range/step;
 
 for i=1:max                               % calculation loop
 
     k_11 = F1(y1(i), y2(i));
     k_21 = F2(y1(i), y2(i));
 
-    k_12 = F1(y1(i) + step * k_11/2, y2(i) + k_21/2 );
-    k_22 = F2(y1(i) + step/2, y2(i) + k_11/2 );
+    k_12 = F1(y1(i) + step * k_11/2, y2(i) + k_21*step/2 );
+    k_22 = F2(y1(i) + step*k_11/2, y2(i) + k_21/2 );
     
-    k_13 = F1(y1(i) + step/2, y2(i) + k_12/2 );
-    k_23 = F2(y1(i) + step/2, y2(i) + k_12/2 );
+    k_13 = F1(y1(i) + step*k_12/2, y2(i) + k_22/2 );
+    k_23 = F2(y1(i) + step*k_12/2, y2(i) + k_22/2 );
     
-    k_14 = F1(y1(i) + step , y2(i)+ k_13);
-    k_24 = F2(y1(i) + step , y2(i)+ k_13);
+    k_14 = F1(y1(i) + step*k_13 , y2(i)+ k_23*step);
+    k_24 = F2(y1(i) + step*k_13 , y2(i)+ k_23*step);
     
     
     y1(i+1) = y1(i) + (1/6)*step*(k_11+2*k_12+2*k_13+k_14);  % main equation
